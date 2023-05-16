191ff584baf471ff309093772e1fd7d5e363931f
bulla1009 <126434656+bulla1009@users.noreply.github.com>
2023-05-16 12:25:55 +0530

    Add files via upload

---
+++ techstore/store/views.py
@@ -0,0 +1,5 @@
+from django.shortcuts import render
+from .models import Product
+def product_detail(request,slug):
+    product=Product.objects.get(slug=slug)
+    return render(request,'store/product_detail.html',{'product':product})
